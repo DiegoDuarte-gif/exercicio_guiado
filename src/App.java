ef4d6f61f8cf73a21dc22192c8307a30da343ab6
DiegoDuarte-gif <diegoduarte0n@gmail.com>
2026-04-13 17:57:20 -0300

    exercicio_guiado

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
-public class App {
+public class App { 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int a = scanner.nextInt();
+        System.out.print("Digite outro número inteiro: ");
+        int b = scanner.nextInt();
+        int maior = descobrirMaior(a,b);
+        System.out.println("O maior número é: " + maior);
         scanner.close();
-        
-        
+       } public static int descobrirMaior(int a, int b){
+        if (a>b){
+            return a;
+        } else {
+            return b;
+        }
+      
+       
     }
 }
